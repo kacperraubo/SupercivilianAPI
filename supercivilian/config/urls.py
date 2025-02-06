e379b2a8e195c40f299bd6b46c7c36d85b4321a0
Dawid <dawid87872@gmail.com>
2025-02-06 11:15:50 +0100

    Add `google` package to `INSTALLED_APPS` and `urls.py`

--- supercivilian/config/urls.py
+++ supercivilian/config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
+# fmt: off
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("google/", include("supercivilian.google.urls")),
 ]
+# fmt: on
