cc1a982435305eafb0c2512bb4a5e8b79d2ac1d8
Dawid <dawid87872@gmail.com>
2025-02-07 10:53:45 +0100

    Create `get_absolute_url` method on `Shelter` model

--- supercivilian/shelters/models.py
+++ supercivilian/shelters/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Shelter(models.Model):
@@ -10,3 +11,6 @@ class Shelter(models.Model):
         return (
             f"Shelter {self.id} ({self.capacity - self.occupancy}/{self.capacity} free)"
         )
+
+    def get_absolute_url(self) -> str:
+        return reverse("shelters:detail", kwargs={"id": self.id})
