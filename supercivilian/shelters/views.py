8f2b236e0625e441c23749fcc637be54b6ad9346
Dawid <dawid87872@gmail.com>
2025-02-07 11:29:50 +0100

    Create `ShelterListView`

--- supercivilian/shelters/views.py
+++ supercivilian/shelters/views.py
@@ -1,9 +1,14 @@
+import typing
+from django.core.cache import cache
 from django.forms import model_to_dict
 from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, TemplateView
 
-from supercivilian.arcgis.utilities import get_details_for_shelter
+from supercivilian.arcgis.utilities import (
+    get_details_for_shelter,
+    get_shelters_from_cache,
+)
 from supercivilian.core.responses import (
     APIErrorResponse,
     APIResponse,
@@ -11,6 +16,7 @@ from supercivilian.core.responses import (
 )
 
 from .models import Shelter
+from .constants import WARSAW_COORDINATES
 
 
 class ShelterDetailView(View):
@@ -39,3 +45,19 @@ class ShelterAPIDetailView(View):
             return APIErrorResponse(status=404)
 
         return APISuccessResponse(payload=model_to_dict(shelter))
+
+
+class ShelterListView(TemplateView):
+    template_name = "shelters/list.html"
+
+    def get_context_data(self, **kwargs: typing.Any) -> dict[str, typing.Any]:
+        context = super().get_context_data(**kwargs)
+
+        if (
+            shelters := get_shelters_from_cache(
+                WARSAW_COORDINATES.longitude, WARSAW_COORDINATES.latitude
+            )
+        ) is not None:
+            context["shelters"] = shelters
+
+        return context
