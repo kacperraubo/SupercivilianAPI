8707b1bd6b899402426b9b82e32b946913274d2f
Dawid <dawid87872@gmail.com>
2025-02-07 11:30:02 +0100

    Add `ShelterListView` to `urls.py`

--- supercivilian/shelters/urls.py
+++ supercivilian/shelters/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import ShelterAPIDetailView, ShelterDetailView
+from .views import ShelterAPIDetailView, ShelterDetailView, ShelterListView
 
 app_name = "shelters"
 
 # fmt: off
 urlpatterns = [
+    path("", ShelterListView.as_view(), name="list"),
     path("<int:id>", ShelterDetailView.as_view(), name="detail"),
     path("api/<int:id>", ShelterAPIDetailView.as_view(), name="api-detail"),
 ]
