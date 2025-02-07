ef8ca590057ee402b1b536e378d1ad06b75c5382
Dawid <dawid87872@gmail.com>
2025-02-07 11:15:08 +0100

    Declare `ALLOWED_HOSTS` in development settings

--- supercivilian/config/settings/development.py
+++ supercivilian/config/settings/development.py
@@ -1,9 +1,11 @@
 from .base import *  # noqa: F403
+from .environment import environment
 from .partial.database import *  # noqa: F403
 from .partial.email import *  # noqa: F403
 from .partial.google import *  # noqa: F403
 
 SECRET_KEY = "development-key-dont-use-this-in-production"
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"] + environment("ALLOWED_HOSTS", "").split(",")
 
 # Caching settings
 
