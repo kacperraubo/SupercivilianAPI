f2959864f202072572f4a7e6e8089aeb2cd49a7b
Dawid <dawid87872@gmail.com>
2025-02-10 10:47:26 +0100

    Add `__str__` and `distance` implementations for `Point` dataclass

--- supercivilian/core/dataclasses.py
+++ supercivilian/core/dataclasses.py
@@ -1,7 +1,40 @@
+from __future__ import annotations
+
 import dataclasses
+
+from geopy.distance import geodesic
 
 
 @dataclasses.dataclass
 class Point:
+    """A point on the Earth.
+
+    Attributes:
+        longitude: The longitude of the point.
+        latitude: The latitude of the point.
+    """
+
     longitude: float
     latitude: float
+
+    def __str__(self) -> str:
+        """The string representation of the point.
+
+        Returns:
+            A string representation of the point.
+        """
+        return f"{self.longitude},{self.latitude}"
+
+    def distance(self, point: Point) -> float:
+        """Get the distance between this point and another point.
+
+        Args:
+            point: The point to get the distance to.
+
+        Returns:
+            The distance between the two points in meters.
+        """
+        return geodesic(
+            (self.latitude, self.longitude),
+            (point.latitude, point.longitude),
+        ).meters
