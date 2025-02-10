a06ea189eb5ce10259f31d3cd3aa9f43ae53a556
Dawid <dawid87872@gmail.com>
2025-02-10 09:53:41 +0100

    Update `sass_development.ps1` and `sass_production.ps1` `--load-path`

--- scripts/sass_development.ps1
+++ scripts/sass_development.ps1
@@ -1,1 +1,1 @@
-sass static/styles/sass/main.sass static/styles/css/main.css --watch
+sass --load-path=./static/styles static/styles/sass/main.sass static/styles/css/main.css --watch
--- scripts/sass_production.ps1
+++ scripts/sass_production.ps1
@@ -1,1 +1,1 @@
-sass static/styles/sass:static/styles/css --style compressed --no-source-map
+sass --load-path=./static/styles static/styles/sass/main.sass static/styles/css/main.css --style compressed --no-source-map
