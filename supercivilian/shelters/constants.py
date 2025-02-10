4e55fa2831e2217bbe3196098395863898537239
Dawid <dawid87872@gmail.com>
2025-02-10 11:11:32 +0100

    Remove `supercivilian.shelters`

--- supercivilian/shelters/constants.py
+++
@@ -1,3 +0,0 @@
-from supercivilian.core.dataclasses import Point
-
-WARSAW_COORDINATES = Point(longitude=21.017532, latitude=52.237049)
--- supercivilian/shelters/dataclasses.py
+++
@@ -1,7 +0,0 @@
-import dataclasses
-
-
-@dataclasses.dataclass
-class Point:
-    longitude: float
-    latitude: float
