2cef0eb8ae60e0704d0073a28db360ea1607b479
Dawid <dawid87872@gmail.com>
2025-02-11 11:07:54 +0100

    Create serializer to `Shelter` objects

---
+++ supercivilian/arcgis/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+
+class ShelterSerializer(serializers.Serializer):
+    """Serialize for `Shelter` objects."""
+
+    id = serializers.IntegerField()
+    longitude = serializers.FloatField()
+    latitude = serializers.FloatField()
+    distance = serializers.FloatField(required=False)
+    inventory_type = serializers.CharField(required=False)
+    access_type = serializers.CharField(required=False)
+    area = serializers.IntegerField(required=False)
+    capacity = serializers.IntegerField(required=False)
+    quality = serializers.IntegerField(required=False)
+    category = serializers.CharField(required=False)
+    purpose = serializers.CharField(required=False)
+    voivodeship = serializers.CharField(required=False)
+    province = serializers.CharField(required=False)
+    address = serializers.CharField(required=False)
