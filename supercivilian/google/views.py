fa437abdda170d76bfa7687a2d1271119580cd50
Dawid <dawid87872@gmail.com>
2025-02-11 09:07:05 +0100

    Update `google` app views to use `APIView`

--- supercivilian/google/views.py
+++ supercivilian/google/views.py
@@ -1,22 +1,34 @@
-from django.views import View
-from django.http import HttpRequest
-
 import requests
+from rest_framework.request import Request
+from rest_framework.views import APIView
 
+from supercivilian.core.params import ParameterError, SearchParameters
 from supercivilian.core.responses import (
-    APIResponse,
     APIErrorResponse,
+    APIResponse,
     APISuccessResponse,
 )
-from supercivilian.core.params import SearchParameters, ParameterError
 
 from .utilities import generate_places_api_url
 
 
-class SearchAutoCompleteView(View):
-    """Proxy view for the Google Places API autocomplete endpoint."""
+class SearchAutoCompleteView(APIView):
+    """GET a list of places matching the query in Poland.
 
-    def get(self, request: HttpRequest) -> APIResponse:
+    Expects a `query` url parameter that contains the search query.
+
+    Note the prediction details are returned in Polish.
+
+    Responses:
+        - 200: A list of places matching the query.
+            - payload: A list of predictions. See [here](https://developers.google.com/maps/documentation/places/web-service/autocomplete#place_autocomplete_responses)
+              for more information about the format of a prediction.
+        - 400: Invalid query parameter.
+        - 404: No places found.
+        - 500: Internal server error.
+    """
+
+    def get(self, request: Request) -> APIResponse:
         parameters = SearchParameters(request)
 
         try:
@@ -44,10 +56,22 @@ class SearchAutoCompleteView(View):
         return APISuccessResponse(payload=payload.get("predictions"))
 
 
-class PlaceDetailsView(View):
-    """Proxy view for the Google Places API details endpoint."""
+class PlaceDetailsView(APIView):
+    """GET details for a place.
 
-    def get(self, request: HttpRequest, id: str) -> APIResponse:
+    Expects an `id` url parameter that contains the place id.
+
+    Note the details are returned in Polish.
+
+    Responses:
+        - 200: Details for the place.
+            - payload: Details for the place. See [here](https://developers.google.com/maps/documentation/places/web-service/details#PlacesDetailsResponse)
+              for more information about the format of a place details response.
+        - 404: Place not found.
+        - 500: Internal server error.
+    """
+
+    def get(self, request: Request, id: str) -> APIResponse:
         url = generate_places_api_url(
             "/details/json",
             place_id=id,
