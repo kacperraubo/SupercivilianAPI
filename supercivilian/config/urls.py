d78f25ec50d1425039f45ba87f1d6b7b7382a2df
Dawid <dawid87872@gmail.com>
2025-02-11 10:02:12 +0100

    Declare schema urls

--- supercivilian/config/urls.py
+++ supercivilian/config/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularRedocView, SpectacularAPIView
 
 # fmt: off
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("google/", include("supercivilian.google.urls")),
     path("arcgis/", include("supercivilian.arcgis.urls")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "openapi/",
+        SpectacularRedocView.as_view(),
+        name="openapi-schema",
+    ),
 ]
 # fmt: on
